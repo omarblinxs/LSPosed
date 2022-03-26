c7fdae2a35962d8f53182744608ad7ce63298237
vvb2060 <vvb2060@gmail.com>
2022-03-26 14:29:02 +0800

    1

--- daemon/src/main/java/org/lsposed/lspd/service/ConfigFileManager.java
+++ daemon/src/main/java/org/lsposed/lspd/service/ConfigFileManager.java
@@ -374,12 +374,14 @@ public class ConfigFileManager {
         if (preLoadedDexes.isEmpty()) return null;
         if (moduleClassNames.isEmpty()) return null;
 
-        for (int i = 0; i < moduleClassNames.size(); i++) {
-            var s = moduleClassNames.get(i);
-            if (s.startsWith("de.robv.android.xposed.")) {
-                var signature = ObfuscationManager.getObfuscatedSignature();
-                var name = signature.substring(1).replace("/", ".");
-                moduleClassNames.add(i, s.replace("de.robv.android.xposed.", name));
+        if (obfuscate) {
+            for (int i = 0; i < moduleClassNames.size(); i++) {
+                var s = moduleClassNames.get(i);
+                if (s.startsWith("de.robv.android.xposed.")) {
+                    var signature = ObfuscationManager.getObfuscatedSignature();
+                    var name = signature.substring(1).replace("/", ".");
+                    moduleClassNames.add(i, s.replace("de.robv.android.xposed.", name));
+                }
             }
         }
 
